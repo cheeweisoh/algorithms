f9b20f614103dcd8f982d42bf91ab3a2957a5826
cheewei <sohcheewei@u.nus.edu>
2023-12-08 22:03:44 +0800

    leetcode daily 2023-12-08

--- Leetcode/Daily Challenge/0606_2023-12-08.py
+++ Leetcode/Daily Challenge/0606_2023-12-08.py
@@ -1,24 +1,8 @@
-import collections
-import math
-import random
-import heapq
-import string
-
 class TreeNode():
     def __init__(self, val = 0, left = None, right = None):
         self.val = val
         self.left = left
         self.right = right
-
-def createTree(nodeList, index):
-    root = TreeNode(nodeList[index])
-    
-    if 2 * index + 1 < len(nodeList):
-        root.left = createTree(nodeList, 2 * index + 1)
-    if 2 * index + 2 < len(nodeList):
-        root.right = createTree(nodeList, 2 * index + 2)
-        
-    return root
 
 class Solution:
     def addtostr(self, node: TreeNode, ans: str) -> str:
@@ -43,9 +27,3 @@ class Solution:
         ans = self.addtostr(root, str(root.val))
         ans = ans.replace('None', '')
         return ans
-
-soln = Solution()
-root1 = createTree([1,2,3,4], 0)
-root2 = createTree([1,2,3,None,4], 0)
-print(soln.tree2str(root1))
-print(soln.tree2str(root2))
