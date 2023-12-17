484fcd779e507953269b42f313191db4caf4e9c6
cheewei <sohcheewei@u.nus.edu>
2023-12-17 12:44:56 +0800

    updated boilerplate

--- AdventOfCode2023/boilerplate.py
+++ AdventOfCode2023/boilerplate.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
-import re
 
 def parseInput(file: list):
     pass
@@ -10,8 +9,16 @@ def main():
     here = Path(__file__).parent
     with open(here/'test.txt') as file:
         test_data = file.read().splitlines()
+        test = parseInput(test_data)
+    
+    with open(here/'puzzle.txt') as file:
+        puzzle_data = file.read().splitlines()
+        puzzle = parseInput(puzzle_data)
     
     print(test_data)
+    
+    # print(f'Test Answer: {}')
+    # print(f'Puzzle Answer: {}')
 
 if __name__ == '__main__':
     main()
