9e836fa6a25ad2d1059d0ce4dd16edcc99cba196
cheewei <sohcheewei@u.nus.edu>
2023-12-21 17:07:08 +0800

    aoc day 19 part 2

--- AdventOfCode2023/Day19/Day19_02.py
+++ AdventOfCode2023/Day19/Day19_02.py
@@ -24,10 +24,10 @@ def change_conds(cond: str, curr_set: dict[str, list[int]]):
     
     if sym == '<':
         pass_set[cat][1] = num - 1
-        fail_set[cat][0] = num + 1
+        fail_set[cat][0] = num
     elif sym == '>':
         pass_set[cat][0] = num + 1
-        fail_set[cat][1] = num - 1
+        fail_set[cat][1] = num
     
     return pass_set, fail_set
 
@@ -36,39 +36,45 @@ def checkWorkflow(workflows: dict[str, list[str]]):
     start_wf = workflows['in']
     starting_set = {'x': [1, 4000], 'm': [1, 4000], 'a': [1, 4000], 's': [1, 4000]}
     final_set = []
-    
-    def dfs(curr_wf, curr_set):
-        # print(curr_set)
-        # print(curr_wf)
-        # print(curr_wf[curr_step])
-        # print(list(workflows.keys())[list(workflows.values()).index(curr_wf)])
-        for i in range(len(curr_wf)):
-            curr_wf_step = curr_wf[i]
-
-            if len(curr_wf_step) == 1:
-                rslt = curr_wf_step[0]
-                pass_set = {key: value[:] for key, value in curr_set.items()}
-            else:
-                cond, rslt = curr_wf_step
-                pass_set, fail_set = change_conds(cond, curr_set)
-        
-            if rslt == 'A':
-                print('accepted', pass_set)
-                final_set.append(pass_set)
-                continue
-            elif rslt == 'R':
-                continue
-            else:
-                dfs(workflows[rslt], pass_set)
-    
-    dfs(start_wf, starting_set)
-    
     ans = 0
-    for s in final_set:
-        combi = 1
-        for start, end in s.values():
-            combi = combi * (end - start + 1)
-        ans += combi
+    
+    def dfs(curr_wf, curr_step, curr_set):
+        nonlocal ans
+        
+        if curr_step == len(curr_wf):
+            return
+        
+        curr_wf_step = curr_wf[curr_step]
+        
+        if curr_step != len(curr_wf) - 1:
+            cond, rslt = curr_wf_step
+            pass_set, fail_set = change_conds(cond, curr_set)
+            
+            if rslt in ['A', 'R']:
+                if rslt == 'A':
+                    combi = 1
+                    for start, end in pass_set.values():
+                        combi *= (end - start + 1)
+                    ans += combi
+                dfs(curr_wf, curr_step+1, fail_set)
+            else:
+                dfs(workflows[rslt], 0, pass_set)
+                dfs(curr_wf, curr_step+1, fail_set)
+        else:
+            rslt = curr_wf_step[0]
+            
+            if rslt in ['A', 'R']:
+                if rslt == 'A':
+                    combi = 1
+                    for start, end in curr_set.values():
+                        combi *= (end - start + 1)
+                    ans += combi
+            else:
+                dfs(workflows[rslt], 0, curr_set)
+        
+        pass
+    
+    dfs(start_wf, 0, starting_set)
     
     return ans
 
@@ -83,14 +89,12 @@ def main():
         puzzle_data = file.read().splitlines()
         puzzle_workflows = parseInput(puzzle_data)
 
-    # print(test_workflows)
-    print(checkWorkflow(test_workflows))
-    # print(f"Test Answer: {checkAllParts(test_parts, test_workflows)}")
-    # print(f"Puzzle Answer: {checkAllParts(puzzle_parts, puzzle_workflows)}")
+    print(f"Test Answer: {checkWorkflow(test_workflows)}")
+    print(f"Puzzle Answer: {checkWorkflow(puzzle_workflows)}")
 
 
 if __name__ == "__main__":
     main()
 
 # 167_409_079_868_000
-# 496_534_091_000_000
+# 167_245_503_449_662
