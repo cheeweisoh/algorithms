602c2c6fcf26addba8f23c3e3202fd188c7fbf0f
cheewei <sohcheewei@u.nus.edu>
2024-03-06 22:53:26 +0800

    daily 2024-03-06

---
+++ Leetcode/DailyChallenge2024/0141_2024-03-06.java
@@ -0,0 +1,30 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
