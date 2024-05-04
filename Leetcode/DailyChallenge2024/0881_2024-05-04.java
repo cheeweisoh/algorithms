2f493aa8236a86460b26a035e86ac7e556f0265e
cheewei <sohcheewei@u.nus.edu>
2024-05-04 12:31:36 +0800

    daily 2024-05-04

---
+++ Leetcode/DailyChallenge2024/0881_2024-05-04.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int ans = 0, l = 0, r = people.length - 1;
+
+        while (l <= r) {
+            if (people[l] + people[r] <= limit) {
+                l++;
+                r--;
+            } else {
+                r--;
+            }
+            ans++;
+        }
+
+        return ans;
+    }
+}
