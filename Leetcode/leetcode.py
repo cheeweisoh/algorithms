8cbec552391876fe11db468516a91a853e29a60d
cheewei <sohcheewei@u.nus.edu>
2024-05-05 12:05:10 +0800

    daily 2024-05-05

--- Leetcode/leetcode.py
+++ Leetcode/leetcode.py
@@ -4,29 +4,15 @@ import random
 import heapq
 import string
 import bisect
+from structures import *
 
 class Solution:
-    def kInversePairs(self, n: int, k: int) -> int:
-        MOD = 10**9 + 7
-        dp = [[0] * (k + 1) for _ in range(n + 1)]
-
-        for i in range(1, n + 1):
-            for j in range(k + 1):
-                if j == 0:
-                    dp[i][j] = 1
-                else:
-                    val = (dp[i - 1][j] + MOD - (dp[i - 1][j - i] if j - i >= 0 else 0)) % MOD
-                    dp[i][j] = (dp[i][j - 1] + val) % MOD
-
-        return (dp[n][k] + MOD - (dp[n][k - 1] if k > 0 else 0)) % MOD
+    def deleteNode(self, node):
+        node.val = node.next.val
+        node.next = node.next.next
 
 def main():
-    soln = Solution()
-    for n, k in [
-        [3, 0],
-        [3, 1],
-    ]:
-        print(soln.kInversePairs(n, k))
+    pass
 
 if __name__ == "__main__":
     main()
