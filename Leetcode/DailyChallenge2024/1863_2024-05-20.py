556dfa2c01c4f61bb1eedc4f6f2a1ee3f60bf541
cheeweisoh <cheewei@Zenbook>
2024-05-20 23:55:19 +0800

    daily 2024-05-20

--- Leetcode/DailyChallenge2024/1863_2024-05-20.py
+++ Leetcode/DailyChallenge2024/1863_2024-05-20.py
@@ -1,13 +1,3 @@
-import collections
-
-# import math
-# import random
-import heapq
-
-# import string
-# import bisect
-# from structures import *
-
 
 class Solution:
     def subsetXORSum(self, nums: list[int]) -> int:
@@ -16,20 +6,9 @@ class Solution:
             if idx == len(nums):
                 return curr
 
-            print(nums[idx])
             include = dfs(idx + 1, curr ^ nums[idx])
             exclude = dfs(idx + 1, curr)
 
             return include + exclude
 
         return dfs(0, 0)
-
-
-def main():
-    soln = Solution()
-    nums = [5, 1, 6]
-    print(soln.subsetXORSum(nums))
-
-
-if __name__ == "__main__":
-    main()
