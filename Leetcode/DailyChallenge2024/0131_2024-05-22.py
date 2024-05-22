cbbea547530fcc8ac136c6f7f27d218de924333e
cheeweisoh <cheewei@Zenbook>
2024-05-23 00:14:10 +0800

    daily 2024-05-22

---
+++ Leetcode/DailyChallenge2024/0131_2024-05-22.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        return s == s[::-1]
+    
+    def partition(self, s: str) -> list:
+        ans = []
+        
+        def dfs(i, curr) -> None:
+            nonlocal ans
+            if i == len(s):
+                ans.append(curr)
+                return
+            
+            for j in range(i, len(s)):
+                next = s[i:j+1]
+                if self.isPalindrome(next):
+                    dfs(j+1, curr + [s[i:j+1]])
+                    
+        dfs(0, [])
+        return ans
