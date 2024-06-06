e80c513dd5aa7c9fad42b2cdfadb2cb772fc4f48
cheeweisoh <cheewei@Zenbook>
2024-06-07 07:52:42 +0800

    daily 2024-06-06

---
+++ Leetcode/DailyChallenge2024/0846_2024-06-06.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        
+        count = Counter(hand)
+        sortedKeys = sorted(count.keys())
+
+        for key in sortedKeys:
+            if count[key] > 0:
+                for i in range(1, groupSize):
+                    if count[key + i] < count[key]:
+                        return False
+                    count[key + i] -= count[key]
+        
+        return True
