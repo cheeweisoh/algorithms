9ca1c9f1fae2e52f473488ca84082d75c9e050a6
cheeweisoh <cheewei@Zenbook>
2024-06-12 14:48:21 +0800

    daily 2024-06-12
    sort colors
    array, two pointers, sorting

--- Leetcode/DailyChallenge2024/0075_2024-06-12.py
+++ Leetcode/DailyChallenge2024/0075_2024-06-12.py
@@ -1,19 +1,8 @@
-# import collections
-# import math
-# import random
-# import heapq
-
-# import string
-# import bisect
-# from structures import *
-
-
 class Solution:
     def sortColors(self, nums: list[int]) -> None:
         count, idx = 0, 0
 
         while count < len(nums):
-            print(count, idx, nums)
             if nums[idx] == 0:
                 nums.pop(idx)
                 nums.insert(0, 0)
@@ -24,14 +13,3 @@ class Solution:
             else:
                 idx += 1
             count += 1
-
-
-def main():
-    soln = Solution()
-    nums = [1, 2, 0]
-    soln.sortColors(nums)
-    print(nums)
-
-
-if __name__ == "__main__":
-    main()
