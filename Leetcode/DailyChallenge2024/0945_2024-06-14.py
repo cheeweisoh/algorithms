180c3590a563700d018f0bb2026a240e6cec825f
cheeweisoh <cheewei@Zenbook>
2024-06-14 22:00:26 +0800

    daily 2024-06-14
    minimum increment to make array unique
    array, greedy, sorting, counting

--- Leetcode/DailyChallenge2024/0945_2024-06-14.py
+++ Leetcode/DailyChallenge2024/0945_2024-06-14.py
@@ -1,13 +1,3 @@
-# import collections
-# import math
-# import random
-# import heapq
-
-# import string
-# import bisect
-# from structures import *
-
-
 class Solution:
     def minIncrementForUnique(self, nums: list[int]) -> int:
         nums.sort()
@@ -17,16 +7,5 @@ class Solution:
             if nums[i] <= nums[i - 1]:
                 ans += nums[i - 1] - nums[i] + 1
                 nums[i] = nums[i - 1] + 1
-            print(nums)
 
         return ans
-
-
-def main():
-    soln = Solution()
-    nums = [3, 2, 1, 2, 1, 7]
-    print(soln.minIncrementForUnique(nums))
-
-
-if __name__ == "__main__":
-    main()
