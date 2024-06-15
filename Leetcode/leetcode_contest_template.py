1003958d75ddf109c2ea1e645e7c721235d27966
cheeweisoh <cheewei@Zenbook>
2024-06-15 12:30:22 +0800

    updated contest template

---
+++ Leetcode/leetcode_contest_template.py
@@ -0,0 +1,49 @@
+import collections
+import math
+import itertools
+import heapq
+import structures
+
+
+####################################################################################
+
+
+class Solution:
+    pass
+
+
+print("Question 1")
+soln = Solution()
+
+
+####################################################################################
+
+
+class Solution:
+    pass
+
+
+print("\nQuestion 2")
+soln = Solution()
+
+
+####################################################################################
+
+
+class Solution:
+    pass
+
+
+print("\nQuestion 3")
+soln = Solution()
+
+
+####################################################################################
+
+
+class Solution:
+    pass
+
+
+print("\nQuestion 4")
+soln = Solution()
