4c42faccd91981291a24aca85d38a47582f2f3c4
cheeweisoh <cheewei@Zenbook>
2024-06-22 22:45:24 +0800

    daily 2024-06-22
    count number of nice subarrays
    array, prefix sum

--- Leetcode/DailyChallenge2024/1248_2024-06-22.py
+++ Leetcode/DailyChallenge2024/1248_2024-06-22.py
@@ -1,14 +1,3 @@
-# import collections
-# import math
-
-# import random
-# import heapq
-
-# import string
-# import bisect
-# from structures import *
-
-
 class Solution:
     def numberOfSubarrays(self, nums: list[int], k: int) -> int:
         prefixSum = [0 for _ in range(len(nums) + 1)]
@@ -22,18 +11,8 @@ class Solution:
         for num in nums:
             curr += num
             if curr >= k:
+                # if curr > k, prefixSum[curr-k] give the number of combinations to remove curr-k odd numbers from the front
                 ans += prefixSum[curr - k]
             prefixSum[curr] += 1
 
         return ans
-
-
-def main():
-    soln = Solution()
-    nums = [1, 1, 2, 1, 1]
-    k = 3
-    print(soln.numberOfSubarrays(nums, k))
-
-
-if __name__ == "__main__":
-    main()
