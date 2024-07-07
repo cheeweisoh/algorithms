0776c645e62ead714f4cefa695105c03831b1c5c
cheewei <sohcheewei@u.nus.edu>
2024-07-07 19:45:40 +0800

    daily 2024-07-07
    water bottles
    math, simulation

---
+++ Leetcode/DailyChallenge2024/1518_2024-07-07.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        ans = numBottles
+        while numBottles >= numExchange:
+            temp = numBottles // numExchange
+            ans += temp
+            numBottles = temp + (numBottles % numExchange) 
+
+        return ans
