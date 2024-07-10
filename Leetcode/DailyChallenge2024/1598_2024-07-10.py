2a2aa6f7f6db35edc9b4328fdbcfe984fd8b7c86
cheewei <sohcheewei@u.nus.edu>
2024-07-11 00:02:22 +0800

    daily 2024-07-10
    crawler log file
    arrays, strings

--- Leetcode/DailyChallenge2024/1598_2024-07-10.py
+++ Leetcode/DailyChallenge2024/1598_2024-07-10.py
@@ -1,14 +1,3 @@
-# import collections
-import math
-
-# import random
-# import heapq
-
-# import string
-# import bisect
-# from structures import TreeNode
-
-
 class Solution:
     def minOperations(self, logs: list[str]) -> int:
         count = 0
@@ -23,13 +12,3 @@ class Solution:
                 count += 1
 
         return max(count, 0)
-
-
-def main():
-    soln = Solution()
-    logs = ["./", "wz4/", "../", "mj2/", "../", "../", "ik0/", "il7/"]
-    print(soln.minOperations(logs))
-
-
-if __name__ == "__main__":
-    main()
