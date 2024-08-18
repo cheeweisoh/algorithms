9a026387007936f242857d8c3cc2c1e55dd47fa7
cheewei <sohcheewei@u.nus.edu>
2024-08-19 07:40:22 +0800

    daily 2024-08-18
    ugly number ii
    heap, math

---
+++ Leetcode/DailyChallenge2024/264_2024-08-18.py
@@ -0,0 +1,21 @@
+import heapq
+
+
+class Solution(object):
+    def nthUglyNumber(self, n):
+        primes = [2, 3, 5]
+        uglyHeap = [1]
+        visited = set()
+        visited.add(1)
+
+        for _ in range(n):
+            curr = heapq.heappop(uglyHeap)
+
+            for prime in primes:
+                new_ugly = curr * prime
+
+                if new_ugly not in visited:
+                    heapq.heappush(uglyHeap, new_ugly)
+                    visited.add(new_ugly)
+
+        return curr
