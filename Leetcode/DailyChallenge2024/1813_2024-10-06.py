d1cbf8509e5077e66d2930591e77471ece36f8cb
cheewei <sohcheewei@u.nus.edu>
2024-10-06 11:59:29 +0800

    daily 2024-10-06
    sentence similarity iii
    array, two pointers

--- Leetcode/DailyChallenge2024/1813_2024-10-06.py
+++ Leetcode/DailyChallenge2024/1813_2024-10-06.py
@@ -1,13 +1,3 @@
-# import collections
-# import math
-# import random
-# import heapq
-# import string
-# import bisect
-# from typing import Optional
-# from structures import ListNode, TreeNode
-
-
 class Solution:
     def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
         s1, s2 = sorted(
@@ -22,14 +12,3 @@ class Solution:
             e += 1
 
         return s + e >= len(s1)
-
-
-def main():
-    soln = Solution()
-    s1 = "My name is Hey"
-    s2 = "My Hey"
-    print(soln.areSentencesSimilar(s1, s2))
-
-
-if __name__ == "__main__":
-    main()
