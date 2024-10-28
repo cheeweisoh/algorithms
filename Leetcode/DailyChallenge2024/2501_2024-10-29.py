cce086d858bdb83e77501751d7b04093d71b74a6
cheewei <sohcheewei@u.nus.edu>
2024-10-29 00:06:48 +0800

    daily 2024-10-29
    longest square streak in an array
    hash table

--- Leetcode/DailyChallenge2024/2501_2024-10-29.py
+++ Leetcode/DailyChallenge2024/2501_2024-10-29.py
@@ -1,13 +1,3 @@
-# import collections
-# import math
-# import random
-# import heapq
-# import string
-# import bisect
-# from typing import Optional
-# from structures import ListNode, TreeNode
-
-
 class Solution:
     def longestSquareStreak(self, nums: list[int]) -> int:
         res = 1
@@ -24,13 +14,3 @@ class Solution:
                 mem[i] = 1
 
         return res if res != 1 else -1
-
-
-def main():
-    soln = Solution()
-    nums = [2, 3, 5, 6, 7]
-    print(soln.longestSquareStreak(nums))
-
-
-if __name__ == "__main__":
-    main()
