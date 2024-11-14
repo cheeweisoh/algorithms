a16d77560bac78d352551eaea30bcb2451af91cf
cheewei <sohcheewei@u.nus.edu>
2024-11-14 12:32:47 +0800

    daily 2024-11-14
    minimized maximum of products distributed to any store
    binary search

--- Leetcode/DailyChallenge2024/2064_2024-11-14.py
+++ Leetcode/DailyChallenge2024/2064_2024-11-14.py
@@ -1,12 +1,4 @@
-# import collections
 import math
-
-# import random
-# import heapq
-# import string
-# import bisect
-# from typing import Optional
-# from structures import ListNode, TreeNode
 
 
 class Solution:
@@ -38,14 +30,3 @@ class Solution:
                 res = mid
 
         return res
-
-
-def main():
-    soln = Solution()
-    n = 6
-    quantities = [11, 6]
-    print(soln.minimizedMaximum(n, quantities))
-
-
-if __name__ == "__main__":
-    main()
