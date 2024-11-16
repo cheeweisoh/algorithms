1a073cc21fff18dae8c07eed5f258452edd23f3d
cheewei <sohcheewei@u.nus.edu>
2024-11-16 12:32:36 +0800

    daily 2024-11-16
    find the power of k-size subarrays I
    sliding window

--- Leetcode/DailyChallenge2024/3254_2024-11-16.py
+++ Leetcode/DailyChallenge2024/3254_2024-11-16.py
@@ -1,41 +1,23 @@
-# import collections
-# import math
-# import random
-# import heapq
-# import string
-# import bisect
-# from typing import Optional
-# from structures import ListNode, TreeNode
-
-
 class Solution:
     def resultsArray(self, nums: list[int], k: int) -> list[int]:
+        # time complexity = O(n)
+
+        # if window is 1, return orig list
         if k == 1:
             return nums
 
         win_size = 1
         res = [-1] * (len(nums) - k + 1)
         for i in range(1, len(nums)):
+            # if curr number is consecutive with prev number, increment window
             if nums[i] - nums[i - 1] == 1:
                 win_size += 1
+                # append last element, guranteed to be largest since window should be consecutive
                 if win_size == k:
                     res[i - k + 1] = nums[i]
                     win_size -= 1
+            # reset window if not consecutive, curr number is start of new window
             else:
                 win_size = 1
 
         return res
-
-
-def main():
-    soln = Solution()
-    nums = [1, 2, 3, 4, 3, 2, 5]
-    k = 3
-    print(soln.resultsArray(nums, k))
-    nums = [3, 2, 3, 2, 3, 2]
-    k = 2
-    print(soln.resultsArray(nums, k))
-
-
-if __name__ == "__main__":
-    main()
