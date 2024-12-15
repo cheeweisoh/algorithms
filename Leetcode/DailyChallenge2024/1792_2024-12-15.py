bfcff56b10f618cd14b84b640e5a0fd91e05c9e2
cheewei <sohcheewei@u.nus.edu>
2024-12-15 16:02:31 +0800

    daily 2024-12-15
    maximum average pass ratio
    greedy, heap

--- Leetcode/DailyChallenge2024/1792_2024-12-15.py
+++ Leetcode/DailyChallenge2024/1792_2024-12-15.py
@@ -1,40 +1,33 @@
-# import collections
-# import bisect
-# import math
-# import random
 import heapq
-
-# import string
-# from typing import Optional
-# from structures import ListNode, TreeNode
 
 
 class Solution:
     def maxAverageRatio(self, classes: list[list[int]], extraStudents: int) -> float:
+        """
+        time complexity: O((n+k) log n)
+        n = no of classes
+        k = no of extra students
+
+        first for loop: O(n)
+        second for loop: O(k)
+        heap push and pull: O(log n)
+        """
         h = []
 
+        # push to a heap the change in ratio when adding a new student
         for p, t in classes:
             heapq.heappush(h, (p / t - ((p + 1) / (t + 1)), p, t))
 
+        # pop the class with the largest change and add one student
         for _ in range(extraStudents):
             _, cp, ct = heapq.heappop(h)
             heapq.heappush(
                 h, ((cp + 1) / (ct + 1) - (cp + 2) / (ct + 2), cp + 1, ct + 1)
             )
 
+        # calculate average ratio
         res = 0
         for _, cp, ct in h:
             res += cp / ct
 
         return res / len(classes)
-
-
-def main():
-    soln = Solution()
-    classes = [[2, 4], [3, 9], [4, 5], [2, 10]]
-    extraStudents = 4
-    print(soln.maxAverageRatio(classes, extraStudents))
-
-
-if __name__ == "__main__":
-    main()
